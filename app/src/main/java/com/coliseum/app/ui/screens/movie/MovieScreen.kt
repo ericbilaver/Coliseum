d151a22c38301101ce27a7d4908d96c9c6684ca1
Eric Bilaver <ericbilaver@gmail.com>
2025-05-01 13:57:07 -0400

    add IMAX capability

--- app/src/main/java/com/coliseum/app/ui/screens/movie/MovieScreen.kt
+++ app/src/main/java/com/coliseum/app/ui/screens/movie/MovieScreen.kt
@@ -1,28 +1,27 @@
 package com.coliseum.app.ui.screens.movie
 
+import android.media.Image
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import app.moviebase.tmdb.model.TmdbMovieDetail
 import coil3.compose.AsyncImage
-import com.coliseum.app.TmdbClient
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.coliseum.app.R
 
-// TODO add IMAX section
-// TODO add viewmodel
 
 @Composable
 fun MovieScreen(
@@ -30,6 +29,7 @@ fun MovieScreen(
     movieId: Int?
 ) {
     val movieInfo by viewModel.movieInfo.collectAsState()
+    val formatInfo by viewModel.formatInfo.collectAsState()
     Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
         AsyncImage(
             model = "https://image.tmdb.org/t/p/w500/${movieInfo?.posterImage?.path}",
@@ -39,12 +39,33 @@ fun MovieScreen(
             Text(it.title, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
             Text(text = it.tagline, fontStyle = FontStyle.Italic, textAlign = TextAlign.Center)
             Text("${it.runtime}mins")
+            formatInfo?.let {
+                FormatImageResolver(it)
+            }
             Text(it.overview)
+
         }
     }
 
     LaunchedEffect(Unit) {
         // TMDB setup
         viewModel.getMovieDetails(movieId ?: 0)
+        viewModel.checkMovieFormat(movieId ?: 0)
     }
+}
+
+@Composable
+fun FormatImageResolver(format: MovieFormat) {
+    val imageID = when(format) {
+        MovieFormat.PartialFilm -> R.drawable.imax_70mm_logo
+        MovieFormat.EntirelyFilm -> R.drawable.imax_70mm_logo
+        MovieFormat.PartialDigital -> R.drawable.imax_logo_blue
+        MovieFormat.EntirelyDigital -> R.drawable.imax_logo_blue
+    }
+
+    Image(
+        painter = painterResource(imageID),
+        contentDescription = null,
+        modifier = Modifier.width(100.dp)
+    )
 }
--- app/src/main/java/com/coliseum/app/ui/screens/movie/MovieViewModel.kt
+++ app/src/main/java/com/coliseum/app/ui/screens/movie/MovieViewModel.kt
@@ -3,18 +3,60 @@ package com.coliseum.app.ui.screens.movie
 import androidx.lifecycle.ViewModel
 import app.moviebase.tmdb.model.TmdbMovieDetail
 import com.coliseum.app.TmdbClient
+import com.coliseum.app.ui.screens.movie.MovieFormat.EntirelyDigital
+import com.coliseum.app.ui.screens.movie.MovieFormat.EntirelyFilm
+import com.coliseum.app.ui.screens.movie.MovieFormat.PartialDigital
+import com.coliseum.app.ui.screens.movie.MovieFormat.PartialFilm
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
+enum class MovieFormat {
+    EntirelyFilm, PartialFilm, EntirelyDigital, PartialDigital;
+    companion object {
+        fun stringToFormat(string: String): MovieFormat? {
+            return when (string) {
+                "entirelyDigital" -> EntirelyDigital
+                "partialDigital" -> PartialDigital
+                "entirelyFilm" -> EntirelyFilm
+                "partialFilm" -> PartialFilm
+                else -> null
+            }
+        }
+    }
+}
+
 @HiltViewModel
 class MovieViewModel @Inject constructor(): ViewModel() {
     val movieInfo = MutableStateFlow<TmdbMovieDetail?>(null)
+    val formatInfo = MutableStateFlow<MovieFormat?>(null)
 
     suspend fun getMovieDetails(movieId: Int) {
         // TMDB setup
         movieInfo.value = TmdbClient.tmdb.movies.getDetails(movieId)
+    }
+
+    suspend fun checkMovieFormat(movieId: Int) {
+        val result = Firebase.firestore.collection("test-max-list").document(movieId.toString())
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
+                println(documentSnapshot)
+            }
+            .addOnFailureListener { exception ->
+                println(exception)
+            }
+
+        result
+            .addOnSuccessListener { documentSnapshot ->
+                println(documentSnapshot)
+                formatInfo.value = MovieFormat.stringToFormat(documentSnapshot.data?.get("format").toString())
+
+            }
+            .addOnFailureListener { exception ->
+                println("Error checking document: ${exception.message}")
+            }
     }
 }
